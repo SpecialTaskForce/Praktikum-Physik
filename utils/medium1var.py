88cdc612958ecf220d63d2465ad1c5979903be34
Simon Marti <simon@ceilingcat.ch>
2012-03-08 20:28:25 +0100

    added all raw data

--- utils/medium1var.py
+++ utils/medium1var.py
@@ -1,30 +1,36 @@
-data = [[12.4, 12.3, 12.4, 12.65, 12.45],
- 	[12.55, 12.7, 12.75, 12.7, 12.55],
- 	[12.4, 13.55, 13.25, 13.4, 13.25],
- 	[14.5, 14.5, 14.4, 14.5, 14.5],
- 	[16, 15.8, 15.8, 16, 16.05],
- 	[17.6, 17.55, 17.7,  17.65, 17.5],
- 	[19.35, 19.5, 19.7, 19.45, 19.45]]
+data = [[13.0, 12.9, 12.9, 12.95, 12.9, 13.0, 12.8, 12.85, 12.95, 12.9],
+        [25.5, 25.1, 25.1, 25.05, 25.25, 25.55, 25.3, 25.3, 25.55, 25.45],
+        [11.85, 11.7, 12.0, 11.8, 11.7, 11.8, 11.9, 11.8, 11.95, 11.7],
+        [12.7, 12.8, 13.0, 12.95, 12.65, 12.7, 12.95, 12.7, 12.7, 12.85],
+        [14.95, 15.1, 14.6, 15.0, 15.15, 15.05, 15.1, 15.1, 15.0, 15.0]]
 
+caption = lambda i: "$T_{%s}$" % i
+value = lambda x: "%s s" % x
 
-
-caption = "$t$"
-nr = "\\#"
-m=5
-
-print "\\begin{tabular}{|l|" + "r|"*m + "r|}"
-print "\\hline"
-print "$d$&" + "&".join(["$T_%s$" % (d+1) for d in range(5)]) + "&avg \\\\"
-print "\\hline"
+m = 5
 
 def avg(_list):
-    return sum(_list)/float(len(_list))
+    return "%.2f" % (sum(_list)/float(len(_list)))
 
-for d in range(len(data)):
-    row = data[d]
-    print "%s&%s&%s\\\\" % (d, "&".join(["%s" % t for t in row]), avg(row))
+for row in data:
+    n = len(row)
+    
+    print "\\begin{tabular}{|" + "l|"*m + "}"
+    print "\\hline"
 
-
-
-print "\\hline"
-print "\\end{tabular}"
+    for j in range((n-1)/m+1):
+        captions = ""
+        values = ""
+        for i in range(m):
+            k = j*m+i
+            if k >= n: break
+            captions += "%s&" % caption(k+1)
+            values += "%s&" % value(row[k])
+        print captions[:-1] + "\\\\"
+        print "\\hline"
+        print values[:-1] + "\\\\"
+        print "\\hline"
+        if k < n - 1:
+            print "\\hline"
+    print "\\end{tabular}"
+    print
